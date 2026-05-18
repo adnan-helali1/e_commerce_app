32f9c0c5783eaf0bb61f3d5352f3d4316c2173f4
adnan-helali1 <helaliadnan877@gmail.com>
2026-05-18 17:20:19 +0300

    save token and remamber me

--- lib/app/core/di/dependency_injection.dart
+++ lib/app/core/di/dependency_injection.dart
@@ -1,11 +1,11 @@
 import 'package:B2B/app/core/networking/api_service.dart';
 import 'package:B2B/app/core/networking/dio_factory.dart';
+import 'package:B2B/app/core/services/token_storage_service.dart';
 import 'package:B2B/app/features/auth/data/repos/login_repo.dart';
 import 'package:B2B/app/features/auth/data/repos/register_repo.dart';
 import 'package:B2B/app/features/auth/logic/cubit/login_cubit.dart';
 import 'package:B2B/app/features/auth/logic/cubit/register_cubit.dart';
 import 'package:dio/dio.dart';
-
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
@@ -15,8 +15,15 @@ Future<void> setupGetIt() async {
   Dio dio = await DioFactory.getDio();
   getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
 
+  // Token Storage Service
+  getIt.registerLazySingleton<TokenStorageService>(
+    () => TokenStorageService(),
+  );
+
   // login
-  getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt()));
+  getIt.registerLazySingleton<LoginRepo>(
+    () => LoginRepo(getIt(), tokenStorage: getIt()),
+  );
   getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt()));
 
   // register
