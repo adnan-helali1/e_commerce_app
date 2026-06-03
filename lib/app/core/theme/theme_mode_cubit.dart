0b26d471b9a273d112c082079e7bc0f32eccada5
adnan-helali1 <helaliadnan877@gmail.com>
2026-06-03 04:17:31 +0300

    theme mode system

--- lib/app/core/theme/theme_mode_cubit.dart
+++ lib/app/core/theme/theme_mode_cubit.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ThemeModeCubit extends Cubit<ThemeMode> {
-  ThemeModeCubit() : super(ThemeMode.light);
+  ThemeModeCubit() : super(ThemeMode.system);
 
   void toggle() {
     emit(
