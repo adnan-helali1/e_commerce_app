04daa897934c27b7b3bfea577bbc477f3117bded
adnan-helali1 <helaliadnan877@gmail.com>
2026-06-20 18:06:30 +0300

    fix the ui

--- lib/app/features/ledger/ui/screens/ledger_export_bottm_sheet.dart
+++ lib/app/features/ledger/ui/screens/ledger_export_bottm_sheet.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:B2B/app/core/helpers/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:media_store_plus/media_store_plus.dart';
 import 'package:path_provider/path_provider.dart';
@@ -66,38 +67,41 @@ class LedgerExportBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 12,
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ListTile(
-              leading: const Icon(Icons.visibility_outlined),
-              title: const Text('Preview'),
-              onTap: () async {
-                Navigator.pop(context);
-                await _previewPdf();
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.download_outlined),
-              title: const Text('Save PDF'),
-              onTap: () async {
-                Navigator.pop(context);
-                await _savePdf(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.share_outlined),
-              title: const Text('Share PDF'),
-              onTap: () async {
-                Navigator.pop(context);
-                await _sharePdf();
-              },
-            ),
-          ],
+      child: Material(
+        color: context.cs.surface,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 12,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: const Icon(Icons.visibility_outlined),
+                title: const Text('Preview'),
+                onTap: () async {
+                  Navigator.pop(context);
+                  await _previewPdf();
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.download_outlined),
+                title: const Text('Save PDF'),
+                onTap: () async {
+                  Navigator.pop(context);
+                  await _savePdf(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.share_outlined),
+                title: const Text('Share PDF'),
+                onTap: () async {
+                  Navigator.pop(context);
+                  await _sharePdf();
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
