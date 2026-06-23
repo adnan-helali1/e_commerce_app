d84eec898e34692ab1c917b9d2c329df46ab4db8
adnan-helali1 <helaliadnan877@gmail.com>
2026-06-23 18:55:15 +0300

    the last refactor no problems have neem detected

--- lib/app/core/widgets/app_summary_header.dart
+++ lib/app/core/widgets/app_summary_header.dart
@@ -4,12 +4,6 @@ import 'package:B2B/app/core/theme/textstyles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'package:B2B/app/core/helpers/extensions.dart';
-import 'package:B2B/app/core/helpers/spacing.dart';
-import 'package:B2B/app/core/theme/textstyles.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 class SummaryHeader extends StatelessWidget {
   final String title;
   final String? subtitle;
